99cffdc6c9a98cb5e2d8dab7b66a3e407327e85b
Alex Overbeck <alexoverbeck@gmail.com>
2025-01-10 15:36:31 -0500

    Add membership selector functionality

---
+++ assets/MembershipSelector.js
@@ -0,0 +1,45 @@
+class MembershipSelector extends HTMLElement {
+  constructor() {
+    super();
+
+    this.select = this.querySelector('#membership__selection');
+    this.link = this.querySelector('a');
+
+    this.select.addEventListener('change', (event) => {
+      this.membershipChange(this.select.value);
+    });
+
+    this.link.addEventListener('click', this.navigateToMembership.bind(this));
+  }
+
+  connectedCallback() {
+    if (this.select.value) {
+      this.membershipChange(this.select.value);
+    }
+  }
+
+  //----------------------------------------------------------------------------
+  // Event Handlers
+
+  membershipChange(membership) {
+    this.updateLink(membership);
+  }
+
+  updateLink(url) {
+    this.link.href = url;
+  }
+
+  navigateToMembership(event) {
+    event.preventDefault();
+    const href = this.link.attributes.href.value;
+
+    if (href && href !== "#") {
+      window.location = this.link.href;
+    } else {
+      this.error = "Please select a membership.";
+      console.warn("No membership selected.");
+    }
+  }
+}
+
+customElements.define("membership-selector", MembershipSelector);
